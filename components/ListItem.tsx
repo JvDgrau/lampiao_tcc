8642a2b7a6af9ada091820554950e26b07209f5b
Joao Victor de Melo Rocha <joao.v.de.melo.rocha@avanade.com>
2023-10-24 11:36:51 -0300

    Feat: 2410 - Ajustes no ListItem Home

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -26,40 +26,31 @@ const ListItem: React.FC<ListItemProps> = ({ name, href }) => {
   };
 
   return (
-    <div
-      className="
-    flex 
-    items-center 
-    justify-center 
-"
-    >
+    <div className="flex items-center justify-center p-2">
       <button
         onClick={onClick}
         className="
-        m-5
-        relative 
-        group 
-        flex 
-        flex-col 
-        items-center 
-        justify-between
-        p-1
-        rounded-md 
-        overflow-hidden 
-        bg-gradient-to-b from-indigo-700
-        cursor-pointer 
-        transition-transform transform 
-        hover:scale-105 
-        w-[100px] 
-        h-[100px]
-        border-2 border-white
-    "
+          flex 
+          flex-col 
+          items-center 
+          justify-between 
+          p-4 
+          rounded-md 
+          bg-white
+          shadow-md 
+          transition-transform transform 
+          hover:scale-105 
+          w-[120px] 
+          h-[120px] 
+          border-2 border-white
+          cursor-pointer
+          relative
+        "
       >
-        <p className="font-medium text-white mb-1 truncate">{name}</p>
-        <div className="bg-white h-[1px] w-3/4 self-stretch mx-3"></div>{" "}
-        <span className="font-bold text-indigo-700 bg-white p-1 mt-1 rounded-md">
-          -
-        </span>
+        <div className="absolute top-0 left-0 w-full h-1/2 bg-indigo-700 flex items-center justify-center z-10">
+          <p className="font-medium text-white">{name}</p>
+        </div>
+        <span className="font-bold text-indigo-700 text-2xl mt-12 z-0">10</span>
       </button>
     </div>
   );
