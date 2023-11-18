beaa926f30664dae788e846aee6af85f736720bc
Joao Victor de Melo Rocha <joao.v.de.melo.rocha@avanade.com>
2023-11-18 08:49:57 -0300

    feat: 1811 - Ajuste de responsividade no BookComponente

--- components/BookComponent.tsx
+++ components/BookComponent.tsx
@@ -108,62 +108,70 @@ const BookComponent: FC<BookComponentProps> = ({
 
   return (
     <>
-      <div className="w-1/2 p-8 bg-gradient-to-b to bg-gray-900 from-indigo-700 flex items-center justify-center">
-        <div className="flex flex-col items-center justify-center h-full mt-12">
-          <img
-            src={bookThumbnail}
-            alt="Book Thumbnail"
-            className="transform scale-125 mb-12 rounded-lg"
-          />
-          <p className="text-center">{bookTitle}</p>
-          <div className="mb-6 flex items-center space-x-2">
-            <div className="bg-white px-4 py-1 rounded-md flex items-center space-x-2 mt-6">
-              <span className="text-gray-700 font-semibold">MÉDIA GERAL |</span>
-              <span className="text-black font-bold">{averageRating}</span>
+      <div className="flex flex-wrap">
+        <div className="w-full md:w-1/2 p-4 bg-gradient-to-b to bg-gray-900 from-indigo-700 flex items-center justify-center">
+          <div className="flex flex-col items-center justify-center h-full mt-4 md:mt-12">
+            <img
+              src={bookThumbnail}
+              alt="Book Thumbnail"
+              className="transform scale-125 mb-8 md:mb-12 rounded-lg"
+            />
+            <p className="text-center">{bookTitle}</p>
+            <div className="mb-4 md:mb-6 flex items-center space-x-2">
+              <div className="bg-white px-3 md:px-4 py-1 rounded-md flex items-center space-x-2 mt-4 md:mt-6">
+                <span className="text-gray-700 font-semibold">
+                  MÉDIA GERAL |
+                </span>
+                <span className="text-black font-bold">{averageRating}</span>
+              </div>
+            </div>
+            <RatingComponent />
+          </div>
+        </div>
+        <div className="w-full md:w-1/2 p-4 flex flex-col max-h-64 overflow-y-auto">
+          <div className="flex flex-col flex-grow overflow-y-auto">
+            <div className="mb-4">
+              <h2 className="text-lg md:text-xl font-bold text-black mb-2">
+                Sinopse
+              </h2>
+              <p className="text-xs md:text-sm font-bold text-[#302D2D] mb-2">
+                {showFullDescription
+                  ? bookDescription
+                  : truncatedDescription(bookDescription)}
+              </p>
+              {bookDescription && bookDescription.length > 150 && (
+                <button
+                  onClick={() => setShowFullDescription(!showFullDescription)}
+                  className="text-indigo-500 hover:underline mt-2"
+                >
+                  {showFullDescription ? "Ver menos" : "Ler mais"}
+                </button>
+              )}
+            </div>
+            <div className="bg-[#F4EEE7] flex-grow overflow-y-auto">
+              <h2 className="text-lg md:text-xl text-black font-bold mb-2">
+                Comentários
+              </h2>
+              {comments?.map((comment, index) => (
+                <div key={index} className="mb-2">
+                  <p className="font-medium">{comment.name}</p>
+                  <p>{comment.comment}</p>
+                </div>
+              ))}
             </div>
           </div>
-          <RatingComponent />
+          <button
+            disabled={!user?.id}
+            onClick={handleButtonClick}
+            className={`px-6 md:px-8 py-1 border-2 rounded-md self-center hover:opacity-75 mt-4 ${
+              isBookAdded
+                ? "bg-red-200 text-red-800 border-red-800 hover:bg-red-300"
+                : "bg-indigo-200 text-indigo-800 border-indigo-800 hover:bg-indigo-300"
+            }`}
+          >
+            {isBookAdded ? "Remover Livro" : "Adicionar Livro"}
+          </button>
         </div>
-      </div>
-      <div className="w-1/2 p-8 flex flex-col h-full">
-        <div className="flex flex-col flex-grow overflow-y-auto">
-          <div className="mb-4">
-            <h2 className="text-xl font-bold text-black mb-2">Sinopse</h2>
-            <p className="text-sm font-bold text-[#302D2D] mb-2">
-              {showFullDescription
-                ? bookDescription
-                : truncatedDescription(bookDescription)}
-            </p>
-            {bookDescription && bookDescription.length > 150 && (
-              <button
-                onClick={() => setShowFullDescription(!showFullDescription)}
-                className="text-indigo-500 hover:underline mt-2"
-              >
-                {showFullDescription ? "Ver menos" : "Ler mais"}
-              </button>
-            )}
-          </div>
-          <div className="bg-[#F4EEE7] flex-grow overflow-y-auto">
-            <h2 className="text-xl text-black font-bold mb-2">Comentários</h2>
-            {comments?.map((comment, index) => (
-              <div key={index} className="mb-2">
-                <p className="font-medium">{comment.name}</p>
-                <p>{comment.comment}</p>
-              </div>
-            ))}
-          </div>
-        </div>
-        <button
-          disabled={!user?.id}
-          onClick={handleButtonClick}
-          className={`px-8 py-1 border-2 rounded-md self-center hover:opacity-75 mt-4 ${
-            isBookAdded
-              ? "bg-red-200 text-red-800 border-red-800 hover:bg-red-300"
-              : "bg-indigo-200 text-indigo-800 border-indigo-800 hover:bg-indigo-300"
-          }`}
-        >
-          {isBookAdded ? "Remover Livro" : "Adicionar Livro"}
-        </button>
       </div>
     </>
   );
