055393662f44051807bb75cbed20a1f4e9e7c690
Joao Victor de Melo Rocha <joao.v.de.melo.rocha@avanade.com>
2023-11-18 09:25:50 -0300

    feat: 1811 - Correção das bordas do ListComponent

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -27,24 +27,24 @@ const ListItem: React.FC<ListItemProps> = ({ name, href }) => {
       <button
         onClick={onClick}
         className="
-          flex 
-          flex-col 
-          items-center 
-          justify-between 
-          p-4 
-          rounded-md 
-          bg-white
-          shadow-md 
-          transition-transform transform 
-          hover:scale-105 
-          w-[120px] 
-          h-[120px] 
-          border-2 border-white
-          cursor-pointer
-          relative
-        "
+        flex 
+        flex-col 
+        items-center 
+        justify-between 
+        p-4 
+        rounded-md 
+        bg-white
+        shadow-md 
+        transition-transform transform 
+        hover:scale-105 
+        w-[120px] 
+        h-[120px] 
+        border-2 border-white
+        cursor-pointer
+        relative
+      "
       >
-        <div className="absolute top-0 left-0 w-full h-1/2 bg-indigo-700 flex items-center justify-center z-10">
+        <div className="absolute top-0 left-0 w-full h-1/2 bg-indigo-700 flex items-center justify-center z-10 rounded-t-md">
           <p className="font-medium text-white">{name}</p>
         </div>
         <span className="font-bold text-indigo-700 text-2xl mt-14 z-0">-</span>
